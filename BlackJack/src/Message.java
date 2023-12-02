ea6b4df93531241899cf7acc62d81c0301241424
HasnainKhan01 <89434398+HasnainKhan01@users.noreply.github.com>
2023-12-01 17:58:31 -0800

    implemented a basic message class for both client and server to use.

--- BlackJack/src/Message.java
+++ BlackJack/src/Message.java
@@ -1,7 +1,84 @@
-public class Message {
+import java.io.Serializable;
 
-	public Message() {
-		
-	}
+public class Message implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
+    // Types of Messages
+    public enum MessageType {
+        LOGIN,
+        LOGOUT,
+        FIND_TABLE,
+        BANK_SETTINGS,
+        DEPOSIT,
+        WITHDRAW,
+        CHANGE_NAME,
+        CHANGE_PASSWORD,
+        DELETE_ACCOUNT
+    }
+
+    // Common Fields
+    private MessageType type;
+    private String userId;
+    private String password;
+
+    // Fields specific to certain message types
+    private String newUserName;
+    private String newPassword;
+    private double depositAmount;
+    private double withdrawAmount;
+
+    // Constructors for different message types
+    public Message(MessageType type, String userId, String password) {
+        this.type = type;
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public Message(MessageType type, String userId, String password, String newUserName, String newPassword) {
+        this(type, userId, password);
+        this.newUserName = newUserName;
+        this.newPassword = newPassword;
+    }
+
+    public Message(MessageType type, String userId, String password, double depositAmount) {
+        this(type, userId, password);
+        this.depositAmount = depositAmount;
+    }
+
+    public Message(MessageType type, String userId, String password, double withdrawAmount, boolean isWithdraw) {
+        this(type, userId, password);
+        if (isWithdraw) {
+            this.withdrawAmount = withdrawAmount;
+        }
+    }
+
+    // Getters for fields
+    public MessageType getType() {
+        return type;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNewUserName() {
+        return newUserName;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public double getDepositAmount() {
+        return depositAmount;
+    }
+
+    public double getWithdrawAmount() {
+        return withdrawAmount;
+    }
 }
