968db8ba6d614844c627595289733bef31c66fb5
syedanida <skhader2@horizon.csueastbay.edu>
2023-12-04 14:06:40 -0800

    JUnit test for Deck.java

--- BlackJack/src/DeckTest.java
+++ BlackJack/src/DeckTest.java
@@ -1,6 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 
@@ -9,27 +10,44 @@ import org.junit.jupiter.api.Test;
 class DeckTest {
 
 	@Test
-	void testDeckInitialization() {
+	public void testDeckInitialization() {
 		Deck deck = new Deck();
 
+		// Ensure that the deck is not null
 		assertNotNull(deck);
+
+		// Check that the deck has the expected number of cards (52)
 		assertEquals(52, deck.getCards().size());
 
-		for (int i = 0; i < deck.getCards().size(); i++) {
-			assertEquals(i + 1, deck.getCards().get(i).getCardValue());
+		// Retrieve the cards from the deck
+		ArrayList<Card> cards = deck.getCards();
+
+		// Loop through each card in the deck
+		for (int i = 0; i < cards.size(); i++) {
+			Card card = cards.get(i);
+
+			int cardValue = card.getCardValue();
+
+			// Ensure that the card value is within the valid range (1 to 13)
+			assertTrue(cardValue >= 1 && cardValue <= 13);
 		}
 	}
 
 	@Test
-	void testShuffle() {
+	public void testShuffle() {
+		// Create a new deck
 		Deck deck = new Deck();
+
+		// Create a copy of the original order of cards in the deck
 		ArrayList<Card> originalOrder = new ArrayList<>(deck.getCards());
 
 		deck.shuffle();
 
+		// Check that the deck still has the expected number of cards (52)
 		assertEquals(52, deck.getCards().size());
 
+		// Ensure that the order of cards after shuffling is not equal to the original
+		// order
 		assertNotEquals(originalOrder, deck.getCards());
 	}
-
 }
